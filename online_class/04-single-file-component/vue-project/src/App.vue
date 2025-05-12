cb068edface7228223bd971ee4b47783367b9df3
강한설 <chehan20000@gmail.com>
2025-05-12 15:06:49 +0900

    s

---
+++ online_class/04-single-file-component/vue-project/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+<h1>fsd</h1>
+<MyComponent></MyComponent>
+<MyComponent></MyComponent>
+<MyComponent></MyComponent>
+</template>
+
+<script setup>
+// src 폴더로 시작되는 경로일떄는 @ 붙여서 표현
+  import MyComponent from '@/components/MyComponent.vue'
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ online_class/04-single-file-component/vue-project/src/components/MyComponent.vue
@@ -0,0 +1,15 @@
+<!-- 이름은 파스탈 케이스로 작성 -->
+<template>
+<h2>mymy</h2>
+<p>{{ message }}</p>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const message = ref('sfsdfsd')
+</script>
+
+<style scoped>
+
+</style>
